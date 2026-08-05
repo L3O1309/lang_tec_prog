aed13eb2e6a684c73ddd03e5c45ddaf656b6609e
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-05 11:27:41 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Comentário em bloco*/
+//Comentário de linha
+
+int main(int argc, char *argv[]) {
+	printf("Laverdi bobao\n");//Professor resenhudo
+	printf("Marco errou hahahahhahahahhahaa\n");
+	return 0;
+
+}
