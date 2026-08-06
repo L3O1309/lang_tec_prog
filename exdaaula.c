7055d7382fce7ed07de366fcb597b6a31f59e877
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-06 11:32:21 -0300

    Add files via upload

---
+++ exdaaula.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Aula 2 do professor Dacio, matéria de LTP (linguagem e técnicas de programação) */
+
+int main(int argc, char *argv[]) {
+	int a, b, c;
+	a = 8;
+	b = 19;
+	c = a + b;
+	
+	printf("A soma de %d + %d é igual a %d", a, b, c);
+	
+	return 0;
+}
