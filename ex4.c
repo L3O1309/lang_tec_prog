fd6fd2e12dfa47c8d7968be894bda07ad6b69ff8
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-10 17:23:03 -0300

    Add comments to circle area calculation

--- ex4.c
+++ ex4.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Exerc�cio 4 de LTP */
+/* Exercício 4 de LTP */
 
 int main(int argc, char *argv[]) {
 	float R, area;
-	printf("Digite o raio do c�rculo: ");
+	printf("Digite o raio do círculo: ");
 	scanf("%f", &R);
 	
-	area = 3.14 * (R*R);
-	printf("A �rea do c�rculo �: %f", area);
+	area = 3.14 * (R*R); /*Estou considerando π como 3,14. 
+	Até onde eu vi, não tem um operador aritmetico para potencia, 
+	pesquisei sobre e descobri uma biblioteca, 
+	mas não sei se o exercício permite*/
+	printf("A área do círculo é: %f", area);
 	return 0;
 }
