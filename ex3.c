5749e669f9254e3e8647af67157e50b6252cc30b
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-10 17:13:14 -0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Aula 2 do professor Dacio, matéria de LTP (linguagem e técnicas de programação) */
+/* Exercício 4 da aula do professor Dacio, matéria de LTP (linguagem e técnicas de programação) */
 
 int main(int argc, char *argv[]) {
 	float a, b, c;
