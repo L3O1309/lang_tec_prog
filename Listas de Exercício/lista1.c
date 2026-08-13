4f66e18abacde50e2c0e73670a113afffa660833
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-13 17:43:16 -0300

    Uncomment code for exercises in lista1.c

--- Listas de Exercício/lista1.c
+++ Listas de Exercício/lista1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define pi 3.14159
 
 /* Lista 1 */
 
 int main(int argc, char *argv[]) {
 //Exercício 1
-	int num1, num2, aux; //definimos a variável aux para auxiliar no que vem pela frente
+	/*int num1, num2, aux; //definimos a variável aux para auxiliar no que vem pela frente
 	
-	/*printf("Exercício 1\n");
+	printf("Exercício 1\n");
 	
 	printf("Insira o valor do primeiro número: ");
 	scanf("%d", &num1);
@@ -19,19 +20,19 @@ int main(int argc, char *argv[]) {
 	num1 = num2;
 	num2 = aux;
 	
-	printf("Números invertidos: %d e %d\n", num1, num2);*/
+	printf("Números invertidos: %d e %d\n", num1, num2);
 //Exercício 2
-/*	printf("Exercício 2\n");
+	printf("Exercício 2\n");
 	double nd;
 	
 	printf("Digite um numero double: ");
 	scanf("%lf", &nd);
 	
-	printf("Em notacao cientifica... %e", nd); */
+	printf("Em notacao cientifica... %e", nd); 
 	
 
 //Exercício 3
-	/*printf("Exercicio 3\n");
+	printf("\nExercicio \n");
 	int n;
 	int b1, b2, b3, b4, b5,b6,b7;
 	printf("Digite um número: ");
@@ -57,22 +58,68 @@ int main(int argc, char *argv[]) {
 	
 	b7 = n;
 	
-	printf("Em binário: %d %d %d %d %d %d %d", b1, b2, b3, b4, b5, b6, b7);*/
+	printf("Em binário: %d %d %d %d %d %d %d", b1, b2, b3, b4, b5, b6, b7);
 	
 //Exercício 4
-	printf("Exercício 4");
+	printf("\nExercício 4\n");
 	
 	float sf, vv, com;
+	
 	printf("Digite seu salario fixo: ");
 	scanf("%f", &sf);
 	
 	printf("Digite o valor das suas vendas: ");
 	scanf("%f", &vv);
 	
-	com = vv * 0,15; 
+	com = vv * 0.15; 
 	sf += com;
 	
-	printf("Seu salario fixo + a comissao e igual a: %f", sf);
+	printf("Seu salario fixo + a comissao e igual a: %.2f", sf);
+	
+//Exercício 5
+	printf("\nExercício 5\n");
+	
+	int a, b, c, d, sum, med, prod;
+	
+	printf("Digite 4 numeros: ");
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	
+	sum = a + b + c + d;
+	med =  (a+b+c+d) / 4;
+	prod = a*b*c*d;
+	
+	printf("Soma: %d\nMédia: %d\nProdutorio %d", sum, med, prod);
+	
+//Exercício 6
+
+	printf("\nExercício 6\n");
+	
+	int idadedias, ano, mes, dia, aux;
+	
+	printf("Digite sua idade em dias: ");
+	scanf("%d", &idadedias);
+	
+	ano = idadedias / 365;
+	aux = idadedias % 365;
+	mes = aux / 30;
+	dia = aux % 30;
+	
+	printf("Sua idade é de aproximadamente... %d anos, %d meses e %d dias.", ano, mes, dia);
+	
+//Exercício 7
+	printf("\nExercício 7\n");
+	
+	float raio, vol;
+	
+	printf("Digite o raio: ");
+	scanf("%f", &raio);
+	
+	vol = (4.0/3) * pi * (raio *raio * raio);
+	
+	printf("O volume é: %f", vol);*/
+	
+//Exercício 8
+	printf("Exercício 8");	
 	
 	
 	return 0;
