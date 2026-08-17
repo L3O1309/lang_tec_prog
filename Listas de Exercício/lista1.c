ba7aa23af0feeb91d3f2786f4cc19c4b9e5e800c
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-17 17:06:16 -0300

    Update lista1.c

--- Listas de Exercício/lista1.c
+++ Listas de Exercício/lista1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define pi 3.14159
 
 /* Lista 1 */
 
 int main(int argc, char *argv[]) {
 //Exercício 1
-	/*int num1, num2, aux; //definimos a variável aux para auxiliar no que vem pela frente
+	int num1, num2, aux; //definimos a variável aux para auxiliar no que vem pela frente
 	
 	printf("Exercício 1\n");
 	
@@ -28,15 +29,15 @@ int main(int argc, char *argv[]) {
 	printf("Digite um numero double: ");
 	scanf("%lf", &nd);
 	
-	printf("Em notacao cientifica... %e", nd); 
+	printf("Em notacao cientifica... %e", nd); //%e e %E deixam em notação, a diferença das duas é o tamanho do e aparecendo no terminal
 	
 
 //Exercício 3
-	printf("\nExercicio \n");
+	printf("\nExercicio 3\n");
 	int n;
 	int b1, b2, b3, b4, b5,b6,b7;
 	printf("Digite um número: ");
-	scanf("%d", &n);
+	scanf("%d", &n); //Confesso que quebrei a cabeça, mas entendi que podia aplicar o sistema de conversão em "escadinha" que o Cidão ensinou
 	
 	b1 = n/64;
 	n = n % 64;
@@ -74,7 +75,7 @@ int main(int argc, char *argv[]) {
 	com = vv * 0.15; 
 	sf += com;
 	
-	printf("Seu salario fixo + a comissao e igual a: %.2f", sf);
+	printf("Seu salario fixo + a comissao e igual a: %.2f", sf); //%.nºf significa o número de casas após a vírgula
 	
 //Exercício 5
 	printf("\nExercício 5\n");
@@ -88,13 +89,13 @@ int main(int argc, char *argv[]) {
 	med =  (a+b+c+d) / 4;
 	prod = a*b*c*d;
 	
-	printf("Soma: %d\nMédia: %d\nProdutorio %d", sum, med, prod);
+	printf("Soma: %d\nMédia: %d\nProdutorio %d", sum, med, prod); //Tinha me esquecido o que é um produtório, mas logo lembrei
 	
 //Exercício 6
 
 	printf("\nExercício 6\n");
 	
-	int idadedias, ano, mes, dia, aux2;
+	int idadedias, ano, mes, dia, aux2; //Essa deu um erro engraçado quando fui rodar todas juntas, o bocó aqui esqueceu que já tinha um aux, aí só troquei pra aux2
 	
 	printf("Digite sua idade em dias: ");
 	scanf("%d", &idadedias);
@@ -102,9 +103,9 @@ int main(int argc, char *argv[]) {
 	ano = idadedias / 365;
 	aux2 = idadedias % 365;
 	mes = aux2 / 30;
-	dia = aux2% 30;
+	dia = aux2 % 30;  
 	
-	printf("Sua idade é de aproximadamente... %d anos, %d meses e %d dias.", ano, mes, dia);
+	printf("Sua idade é de aproximadamente... %d anos, %d meses e %d dias.", ano, mes, dia); 
 	
 //Exercício 7
 	printf("\nExercício 7\n");
@@ -114,13 +115,27 @@ int main(int argc, char *argv[]) {
 	printf("Digite o raio: ");
 	scanf("%f", &raio);
 	
-	vol = (4.0/3) * pi * (raio *raio * raio);
+	vol = (4.0/3) * pi * (raio *raio * raio); //Podia usar o pow, mas achei chato
 	
-	printf("O volume é: %f", vol);*/
+	printf("O volume é: %f", vol);
 	
 //Exercício 8
-	printf("Exercício 8");	
+
+	printf("\nExercício 8\n");	
+	float x1, y1, x2, y2, dist, dfinal;
 	
+	printf("Digite a coordenada do ponto 1 (eixo x e eixo y): ");
+	scanf("%f %f", &x1, &y1);
+		
+	printf("Digite a coordenada do ponto 2 (eixo x e eixo y): ");
+	scanf("%f %f", &x2, &y2);
 	
+	dist = (((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));//Poderia ter facilitado com o pow, mas quando percebi deu preguiça de mudar, fica difícil entender vendo, mas se executar o resultado é o mesmo ;)
+	
+	dfinal = sqrt(dist);
+	
+	printf("A distancia entre os pontos é: %.2f", dfinal);
+	
+
 	return 0;
 }
