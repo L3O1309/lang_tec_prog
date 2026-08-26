9d935876213df0dce3f787e4e0662b101973f12c
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-26 11:15:20 -0300

    Update integracaocondicional.c

--- Aula5/integracaocondicional.c
+++ Aula5/integracaocondicional.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <locale.h>
 /* Teve que introduzir um fluxograma, pois 50% da sala não tinha visto e não tinha sido introdizido em algoritmos*/
 
 int main(int argc, char *argv[]) {
+setlocale(LC_ALL, "Portuguese");
 	int a, b, c, r;
 	
 	printf("Digite três valores respectivamente: ");
