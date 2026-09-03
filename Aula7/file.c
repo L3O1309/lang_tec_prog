15d5ba67077b4f095d8cf7a9d1fd4c4cb4f022fa
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-09-03 11:21:04 -0300

    Update file.c

--- Aula7/file.c
+++ Aula7/file.c
@@ -1,1 +1,26 @@
-/*Apenas para criar a pasta ;)*/
+/*Aprendendo Switch e Case*/
+#include <stdio.h>
+#include <stdlib.h> 
+#include <locale.h>
+void v1(){
+  printf("Froes lindão");
+}
+void v2(){
+  printf("Marco lindão");
+}
+void v3(){
+  printf("Leo Maravilhoso");
+}
+int main(int argc, char *argv[]) {
+setlocale(LC_ALL, "Portuguese");
+  int option;
+  printf("Digite um número de 1-3: ");
+  scanf("%d", &option);
+  switch (option){
+    case 1: v1(); break;
+    case 2: v2(); break;
+    case 3: v3(); break;
+    default: printf("Tente novamente!");
+  }
+  return 0;
+}
